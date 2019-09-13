3bfda5831d8cdb7eb0c5abef420e481963c68537
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-12 22:43:14 -0300

    Modificado o arquivo admin.py

--- apartamento/app_apartamento/admin.py
+++ apartamento/app_apartamento/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class ApartamentoAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'qtdQuartos', 'proprietario', 'valor_Condominio')
+
+
+admin.site.register(Apartamento,ApartamentoAdmin)
+
+
+
